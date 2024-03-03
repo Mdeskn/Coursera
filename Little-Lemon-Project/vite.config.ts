d3937e10804231dcffa4844be2f8fc725a234948
mdeskn <maed.es74@gmail.com>
2024-03-03 11:02:23 +0330

    update

---
+++ Little-Lemon-Project/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "mdeskn.github.io/Coursera.github.io/Little-Lemon-Project/"
+})
